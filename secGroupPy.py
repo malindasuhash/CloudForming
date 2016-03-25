7b0f0f0b96ca2b2d434d7f2c48a0b1bb3433d68f
malindasuhash <malinda.suhash@gmail.com>
2016-03-25 15:28:59 +0000

    partially complited python script

--- secGroupPy.py
+++ secGroupPy.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 import boto.ec2
+import hashlib
 
 try:
-  conn = boto.ec2.connect_to_region("us-east-1")
+  conn = boto.ec2.connect_to_region("us-west-2")
   currentSgs = conn.get_all_security_groups()
- except boto.exception.BotoServerError, e:
-  log.error(e.error_message)
- conn.close()
+except boto.exception.BotoServerError as e:
+   log.error(e.error_message)
+conn.close()
  
- for sg in currentSgs
+for sg in currentSgs:
+  print "="*72
   print "id:\t\t", sg.id
   print "name:\t\t", sg.name
   print "vpc:\t\t", sg.vpc_id
   print "instance:\t", sg.instances()
+  for rule in sg.rules:
+    ruledata = sg.id,rule.grants,rule.ip_protocol,rule.from_port,rule.to_port,"$
+    rulehash = hashlib.sha256(str(ruledata)).hexdigest()
+    print "\thash",rulehash
+    print "\t", rule.grants, "-> instances: ", rule.from_port, " - ", rule.to_p$
+  print "="*72
+
